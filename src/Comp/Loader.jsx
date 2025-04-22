ca8982c3e31f48d5e4d7a41131d1bd38697e60f1
mumit-1 <mumitkhan85@gmail.com>
2025-04-23 00:40:55 +0600

    last1

--- src/Comp/Loader.jsx
+++ src/Comp/Loader.jsx
@@ -1,6 +1,43 @@
 import React from 'react';
 import { PropagateLoader } from 'react-spinners';
 const Loader = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return (
         <div className='h-screen w-10 mx-auto pt-44'>
                <PropagateLoader color='#02ffe2'/>
